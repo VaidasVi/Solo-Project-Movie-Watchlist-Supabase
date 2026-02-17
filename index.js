7a564743be67a38bd91e9d8df62fe2fa99698576
Vaidas <32089098+VaidasVi@users.noreply.github.com>
2026-02-17 10:11:40 +0200

    - Added some html

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
 const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
 const searchForm = document.getElementById("search-form");
+const resultContainer = document.getElementById("result-container");
 
 document.querySelector("form").addEventListener("submit", (e) => {
-	e.preventDefault();
+  e.preventDefault();
 
-	let html = "";
-	const searchValue = document.querySelector('input[type="text"]').value;
+  let html = "";
+  const searchValue = document.querySelector('input[type="text"]').value;
 
-	findMovies(searchValue);
+  findMovies(searchValue);
 });
 
 function findMovies(searchvalue) {
-	fetch(`http://www.omdbapi.com/?s=${searchvalue}&apikey=${API_KEY}`)
-		.then((res) => {
-			if (!res.ok) {
-				throw new Error(`HTTP error! status: ${res.status}`);
-			}
-			return res.json();
-		})
-		.then((data) => {
-			// Check if the API returned an error (e.g., no movies found)
-			if (data.Response === "False") {
-				console.error("Error:", data.Error);
-				return;
-			}
+  fetch(`http://www.omdbapi.com/?s=${searchvalue}&apikey=${API_KEY}`)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((data) => {
+      // Check if the API returned an error (e.g., no movies found)
+      if (data.Response === "False") {
+        console.error("Error:", data.Error);
+        return;
+      }
+      // console.log(data.Search);
+      let foundMovieList = [];
 
-			console.log(data.Search);
+      data.Search.map((movie) => {
+        fetch(
+          `http://www.omdbapi.com/?i=${movie.imdbID}&plot=short&apikey=${API_KEY}`,
+        )
+          .then((res) => res.json())
+          .then((data) => foundMovieList.push(data));
+      });
 
-			let foundMovieList = [];
-
-			data.Search.map((movie) => {
-				fetch(
-					`http://www.omdbapi.com/?i=${movie.imdbID}&plot=short&apikey=${API_KEY}`,
-				)
-					.then((res) => res.json())
-					.then((data) => foundMovieList.push(data));
-			});
-
-			console.log(foundMovieList);
-		})
-		.catch((error) => {
-			console.error("Error fetching movies:", error);
-		});
+      console.log(foundMovieList);
+    })
+    .catch((error) => {
+      console.error("Error fetching movies:", error);
+    });
 }
